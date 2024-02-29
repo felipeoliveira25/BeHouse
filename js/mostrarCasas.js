9e4a0ee3bb55eaae0628fb69c549b216a1a8b5e2
felipeoliveira25 <felipeoferreira.25@gmail.com>
2024-02-29 13:39:32 -0300

    atualizações do projeto nos cards das casas

--- js/mostrarCasas.js
+++ js/mostrarCasas.js
@@ -9,9 +9,29 @@ function constroiCardCasa(imagem, titulo, preco){
         <img src="${imagem}" alt="">
         <h3>${titulo}</h3>
         <p>${preco}</p>
-        <button>Buy</button>
+        <div class="botoes-card">
+            <input type="button" value="More Details" class="botao-comprar" data-comprar>
+            <button class="botao-salvar-inativo"><i class="fa-regular fa-bookmark"></i></button>
+            <button class="botao-salvar-ativo hidden" data-salvar><i class="fa-solid fa-bookmark"></i></button>
+            <button class="botao-compartilhar" data-compartilhar><i class="fa-solid fa-share-nodes"></i></button>
+            
+        </div>
     `
-
+    const botaoSalvarInativo = casa.querySelector(".botao-salvar-inativo");
+    const botaoSalvarAtivo = casa.querySelector(".botao-salvar-ativo");
+    botaoSalvarInativo.addEventListener('click', ()=>{
+       if(!botaoSalvarInativo.classList.contains('hidden')){
+            botaoSalvarInativo.classList.add('hidden');
+            botaoSalvarAtivo.classList.remove('hidden')
+       }
+    })
+    botaoSalvarAtivo.addEventListener('click', ()=>{
+        if(!botaoSalvarAtivo.classList.contains('hidden')){
+            botaoSalvarAtivo.classList.add('hidden');
+            botaoSalvarInativo.classList.remove('hidden');
+        }
+    })
+    
     return casa;
 }
 
@@ -23,3 +43,7 @@ async function listaCasas(){
 }
 
 listaCasas();
+
+
+
+
